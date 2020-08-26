ee38dd77eb74959f86a2c9f82fe66c58a364307b
Delgado Lucas (Diseño - lanacion digital) <ldelgado@lanacion.com.ar>
2020-08-26 16:13:10 -0300

    buiild

---
+++ build/precache-manifest.ad2003f2034097d95bebe197abbadbd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c150d085e9b97620c03dcb21982003b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "680cc4d3757c50fc194a",
+    "url": "/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "fdae26e954fd08920a57",
+    "url": "/static/js/2.9d7eb6f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9d7eb6f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680cc4d3757c50fc194a",
+    "url": "/static/js/main.2d4e8fc7.chunk.js"
+  },
+  {
+    "revision": "b35edd16369ef0c40324",
+    "url": "/static/js/runtime-main.a08fd1e6.js"
+  }
+]);
