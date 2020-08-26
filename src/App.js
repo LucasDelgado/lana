5a5c5e5d65cdb7f8836701796b33ead5c376bd3a
Delgado Lucas (Diseño - lanacion digital) <ldelgado@lanacion.com.ar>
2020-08-26 15:56:58 -0300

    se arma ruteo de 2 pages y config

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Switch,Route,Link} from "react-router-dom";
+
+import Home from './pages/home';
+import About from './pages/about';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+            <hr />
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </main>
+      <footer>
+        Hola soy el footer
+      </footer>
     </div>
   );
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = (props) => {
+    return ( 
+        <div>
+            <h1>Hola soy el home</h1>
+        </div>
+     );
+}
+ 
+export default Home;
