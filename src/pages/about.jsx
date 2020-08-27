b8e8d935cd5e036726125797fa05b245c7d414bd
Delgado Lucas (Diseño - lanacion digital) <ldelgado@lanacion.com.ar>
2020-08-27 10:55:14 -0300

    nuevo probamos prod

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = (props) => {
     return ( 
         <div>
-            <h1>Hola soy el about</h1>
+            <h1>Hola soy el about pero ahora estoy nuevo</h1>
         </div>
      );
 }
