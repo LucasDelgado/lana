2fa092c4db8c95c5d476097e37df0a2c71711d2e
Daro <daro@Daros-MacBook-Pro.local>
2020-09-21 13:50:08 -0300

    update :)

---
+++ src/components/com-button.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const getTagProps = (tag, props) => {
+    const { type, value, href, target } = props;
+
+    const propsByTag = {
+        a: { href, target },
+        input: { type: type || 'button', value },
+        button: { type: type || 'button' }
+    };
+
+    return propsByTag[tag];
+
+};
+
+const ComButton = props => {
+    const { children, href, value, mod, icon, size } = props;
+    const className = `com-button ${mod || ``} ${size || ``}`;
+
+    const tag = (href && 'a') || (value && 'input') || 'button';
+
+    const _props = {
+        className,
+        ...getTagProps(tag, props)
+    };
+
+    const _children = children && <span>{children}</span>;
+    const _icon = icon && <i className={`com-icon --${icon}`}>(i)</i>;
+
+    const content = tag !== 'input' ? [_children, _icon] : null;
+
+    const component = React.createElement(tag, { ..._props }, content);
+
+    return _children || _icon || value ? component : null;
+
+};
+
+export default ComButton;
+
+
+
+
---
+++ src/components/com-title.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ComTitle = (props) => {
+    const { tag, href, mod, text, size } = props;
+
+    const _tag = tag || 'h4';
+    //const _content = href ? <a href={href} title={text}>{text}</a> : text;
+    const _content = href ? <a href={href} title={text}>{text} link en {_tag}</a> : `${text} en ${_tag}`;
+    const className = `com-title ${mod || ``} ${size || ``}`;
+
+    const component = React.createElement(_tag, {className}, _content);
+    return component;
+
+}
+ 
+export default ComTitle;
