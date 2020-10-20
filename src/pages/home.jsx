a90101a1ee5c51dbe406ddae5b8f033695348377
Paparella <XPaparella@lanacion.com.ar>
2020-10-20 10:12:30 -0300

    Se modifica la paleta de colores de LANA

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 const Home = (props) => {
     return ( 
         <div>
-            <h1>Indice de Proyectos y páginas</h1>
+            <h1>Indice</h1>
             <h3>Adquisición</h3>
             <h3>Club</h3>
             <h3>LN</h3>
+            <h3>Storybook</h3>
         </div>
      );
 }
