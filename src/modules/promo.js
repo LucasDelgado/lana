094b0dc0b82e7da1f6c3ff0bc0e1f5e5e33c2947
Delgado Lucas (Diseño - lanacion digital) <ldelgado@lanacion.com.ar>
2020-12-02 12:33:19 -0300

    s

--- src/modules/promo.js
+++ src/modules/promo.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from '../components/button';
 import Image from '../components/image';
-import Title from '../components/titles';
+import Title from '../components/title';
 
 
 const Promo  = ( {title, imageUrl, descrip} ) => {
